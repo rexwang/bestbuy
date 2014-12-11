2501d832d0c2223c05bdcfb645b5577e327d6f49
Rex Wang <rexwang@MediaValets-MacBook-Pro.local>
2014-12-10 17:02:54 -0800

    load templates

--- scripts/app/controllers/main.js
+++ scripts/app/controllers/main.js
@@ -1,16 +1,26 @@
 define([
   'jquery',
   'underscore',
-  'app/models/category'
-], function($, _, Category) {
+  'app/models/category',
+  'app/templates/category-list'
+], function($, _, Category, CategoryListView) {
   // do something here
 
   return {
     init: function() {
+
+      var compiled = _.template('<li><%= categoryName %></li>');
+
       // load data
       Category.getCategories().done(function(data) {
-        console.log(data);
+        _.each(data.subCategories, function(category, key, list) {
+          console.log(category.name);
+          console.log(CategoryListView);
+        });
       });
+
+      // var compiled = _.template('<li><%= name %></li>');
+      // $('#category-list').html(compiled({name: 'moe'}));
     }
   };
 });
